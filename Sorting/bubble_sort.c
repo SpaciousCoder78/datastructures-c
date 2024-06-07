e49cc59a1d68a41d036da1b68babb75f77f46682
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-06-07 17:49:13 +0530

    Create bubble_sort.c

---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,25 @@
+//bubble sort
+#include<stdio.h>
+
+int main() {
+    int arr[100],n,i,j,swap;
+    printf("Enter no of elements: ");
+    scanf("%d",&n);
+    printf("Enter %d integers",n);
+    for(i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-1;i++) {
+        for(j=0;j<n-i-1;j++) {
+            if(arr[j]>arr[j+1]) {
+                swap=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=swap;
+            }
+        }
+    }
+    printf("Sorted List in ascending order: ");
+    for(i=0;i<n;i++) {
+        printf("%d ,",arr[i]);
+    }
+}
