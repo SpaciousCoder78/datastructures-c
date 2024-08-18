a8792c8a35dff827e9d2a499b7db9a1d3b94672c
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-08-18 21:30:21 +0530

    Update binary_tree_non_recursive.c

--- Trees/binary_tree_non_recursive.c
+++ Trees/binary_tree_non_recursive.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 
 #define SIZE 20
-int top = -1;
-
 
 typedef struct node {
     int data;
@@ -13,7 +11,36 @@ typedef struct node {
 } BinTree;
 
 BinTree* Stack[SIZE];
-void push(BinTree *t) {
+int top = -1;
+
+BinTree* createNode() {
+    BinTree* newnode;
+    newnode = (BinTree*)malloc(sizeof(BinTree));
+    printf("Enter data: ");
+    scanf(" %d", &newnode->data);
+    newnode->lchild = NULL;
+    newnode->rchild = NULL;
+    return newnode;
+}
+
+BinTree* create() {
+    int ch;
+    BinTree* t;
+    t = createNode();
+    puts("If you want left child press 1");
+    scanf("%d", &ch);
+    if (ch == 1) {
+        t->lchild = create();
+    }
+    puts("For right child press 1: ");
+    scanf("%d", &ch);
+    if (ch == 1) {
+        t->rchild = create();
+    }
+    return t;
+}
+
+void push(BinTree* t) {
     if (top == SIZE - 1) {
         printf("Overflow\n");
     } else {
@@ -31,7 +58,7 @@ BinTree* pop() {
     }
 }
 
-void inorder(BinTree *t) {
+void inorder(BinTree* t) {
     while (1) {
         if (t != NULL) {
             push(t);
@@ -46,7 +73,7 @@ void inorder(BinTree *t) {
     }
 }
 
-void preorder(BinTree *t) {
+void preorder(BinTree* t) {
     while (1) {
         if (t != NULL) {
             printf("%d ", t->data);
@@ -61,8 +88,8 @@ void preorder(BinTree *t) {
     }
 }
 
-void postorder(BinTree *t) {
-    BinTree *prev = NULL;
+void postorder(BinTree* t) {
+    BinTree* prev = NULL;
     while (t != NULL || top != -1) {
         if (t != NULL) {
             push(t);
@@ -81,29 +108,56 @@ void postorder(BinTree *t) {
 }
 
 int main() {
-    // Example usage
-    BinTree* root = (BinTree*)malloc(sizeof(BinTree));
-    root->data = 1;
-    root->lchild = (BinTree*)malloc(sizeof(BinTree));
-    root->rchild = (BinTree*)malloc(sizeof(BinTree));
-    root->lchild->data = 2;
-    root->rchild->data = 3;
-    root->lchild->lchild = NULL;
-    root->lchild->rchild = NULL;
-    root->rchild->lchild = NULL;
-    root->rchild->rchild = NULL;
+    BinTree* root = NULL;
+    int choice;
 
-    printf("Inorder traversal: ");
-    inorder(root);
-    printf("\n");
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Create Binary Tree\n");
+        printf("2. Inorder Traversal\n");
+        printf("3. Preorder Traversal\n");
+        printf("4. Postorder Traversal\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    printf("Preorder traversal: ");
-    preorder(root);
-    printf("\n");
-
-    printf("Postorder traversal: ");
-    postorder(root);
-    printf("\n");
+        switch (choice) {
+            case 1:
+                root = create();
+                break;
+            case 2:
+                if (root == NULL) {
+                    printf("Tree is empty\n");
+                } else {
+                    printf("Inorder Traversal: ");
+                    inorder(root);
+                    printf("\n");
+                }
+                break;
+            case 3:
+                if (root == NULL) {
+                    printf("Tree is empty\n");
+                } else {
+                    printf("Preorder Traversal: ");
+                    preorder(root);
+                    printf("\n");
+                }
+                break;
+            case 4:
+                if (root == NULL) {
+                    printf("Tree is empty\n");
+                } else {
+                    printf("Postorder Traversal: ");
+                    postorder(root);
+                    printf("\n");
+                }
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 
     return 0;
 }
